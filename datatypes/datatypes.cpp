7223c4109171967a49169cf5fc609800f432afe1
imashiqe <imashiqe@gmail.com>
2023-04-14 14:44:34 +0600

    datatypes

---
+++ datatypes/datatypes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a; //declaration
+    a=12; //initialisation
+
+    cout<<"size of int "<<sizeof(a)<<endl;
+
+    float b;
+       cout<<"size of float "<<sizeof(b)<<endl;
+
+       char c;
+         cout<<"size of char "<<sizeof(c)<<endl;
+
+         bool d;
+           cout<<"size of bool "<<sizeof(d)<<endl;
+
+
+    
+ return 0;    
+}
