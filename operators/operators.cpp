7f2b9447bcb505b241b5ae3ad8168b5a9b8cc62e
imashiqe <imashiqe@gmail.com>
2023-07-20 01:42:49 +0600

    post pre increment   decrement

--- operators/operators.cpp
+++ operators/operators.cpp
@@ -15,11 +15,20 @@ int main()
   // cout<<i<<"j "<<" "<<k<<endl;
 
 
-  int i=0;
-  //0 //0 //1  //1
-  i = i++ - -- i + ++i - i--;
- //1
-  cout<<i<<endl;
+//   int i=0;
+//   //0 //0 //1  //1
+//   i = i++ - -- i + ++i - i--;
+//  //1
+//   cout<<i<<endl;
+
+int i=10, j=20, k;
+   //10 //9 //19 //20 //10 //19
+   k = i-- - i++ + --j - ++j + --i - j-- +  ++i - j++;
+
+   cout<<i<<endl;
+   cout<<j<<endl;
+   cout<<k<<endl;
+
 
     return 0;
 }
