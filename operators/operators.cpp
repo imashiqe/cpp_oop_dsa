e590408718d3245b16194f43cb1580aa0ab63adb
imashiqe <imashiqe@gmail.com>
2023-07-20 01:36:02 +0600

    post decrement

--- operators/operators.cpp
+++ operators/operators.cpp
@@ -4,16 +4,22 @@ using namespace std;
 
 int main()
 {
-    int i =1;
-    int j =2;
-    int k;
+  //   int i =1;
+  //   int j =2;
+  //   int k;
 
-    k = i + j + i++ + j++ + ++i + ++j;
+  //   k = i + j + i++ + j++ + ++i + ++j;
 
-    //i = i++ + ++i;
+  //   //i = i++ + ++i;
   
-  cout<<i<<"j "<<" "<<k<<endl;
+  // cout<<i<<"j "<<" "<<k<<endl;
 
+
+  int i=0;
+  //0 //0 //1  //1
+  i = i++ - -- i + ++i - i--;
+ //1
+  cout<<i<<endl;
 
     return 0;
 }
