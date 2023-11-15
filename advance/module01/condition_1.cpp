c0997c523b1115cac5b478ccc47eae496fdbe909
Ashiqur Rahman <imashiqe@gmail.com>
2023-11-15 06:51:21 +0600

    condition of cpp

---
+++ advance/module01/condition_1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int x = 10;
+
+    if(x == 10)
+    {
+         cout << "it is  ten  out of ten\n";
+    }
+    else{
+        cout << "This is not" <<endl;
+    }
+    return 0;
+}
