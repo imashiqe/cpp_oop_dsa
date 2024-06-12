ae014465f55a43b4c0e6ca5242f84b100ce828ea
Ashiqur Rahman <imashiqe@gmail.com>
2024-06-12 13:38:32 +0600

    O of log n

---
+++ datastucture/module01/O_logN.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+
+{
+    int n;
+
+    cin>>n;
+
+    // while (n>0)
+    // {
+    //     int digit=n%10;
+    //     cout<<digit<<endl;
+    //     n/=10;
+
+    // }
+    for (int  i = 1; i <=n; i*2)
+    {
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
