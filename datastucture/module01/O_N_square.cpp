48f8ba5ad872e9cbe41204afa2e1de05332a4553
Ashiqur Rahman <imashiqe@gmail.com>
2024-06-12 14:09:22 +0600

    O(N*N) Time Complexity

---
+++ datastucture/module01/O_N_square.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+   
+
+        int n;
+    cin>>n;
+     for (int i = 0; i <=n-1; i++)
+     {
+        for (int j = i+1; j<n; i++)
+        {
+           cout<<"hello"<<endl;
+        }
+        
+     }
+     
+    return 0;
+}
