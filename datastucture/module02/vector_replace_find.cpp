1c8ac8543a111abce63989ac58b06240e0967666
Ashiqur Rahman <imashiqe@gmail.com>
2024-06-13 15:05:21 +0600

    vector find and replace

--- datastucture/module02/vector_replace_find.cpp
+++ datastucture/module02/vector_replace_find.cpp
@@ -3,19 +3,24 @@
 using namespace std;
 
 int main()
-
 {
-    // replace
-    // vector<int>v={1,2,3,4,5,1,2,4,5,3,2};
-    //  replace(v.begin(),v.end(),2,100);
-    //  for(int x:v)
-    //  {
-    //     cout<<x<<" ";
-    //  }
-    vector<int>v={1,2,3,4,5,1,2,4,5,3,2};
+    // Example to replace all occurrences of 2 with 100
+    // vector<int> v = {1, 2, 3, 4, 5, 1, 2, 4, 5, 3, 2};
+    // replace(v.begin(), v.end(), 2, 100);
+    // for (int x : v)
+    // {
+    //     cout << x << " ";
+    // }
+    // cout << endl;
+
+    vector<int> v = {1, 2, 3, 4, 5, 1, 2, 4, 5, 3, 2};
   
-    auto it = find(v.begin(),v.end,3);
-    if(it == v.end()) cout<<"Not found";
-   else cout<<"Found";
+    // Finding the first occurrence of 3 in the vector
+    auto it = find(v.begin(), v.end(), 3);
+    if (it == v.end())
+        cout << "Not found";
+    else
+        cout << "Found";
+    
     return 0;
 }
