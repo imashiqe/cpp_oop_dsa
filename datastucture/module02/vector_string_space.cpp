34083cab6082b426c8c27fb01e05b11859bc909d
Ashiqur Rahman <imashiqe@gmail.com>
2024-06-13 15:44:25 +0600

    vector space string

---
+++ datastucture/module02/vector_string_space.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    cin.ignore();
+    for(int i=0; i<n; i++)
+    {
+        
+        getline(cin,v[i]);
+    }
+
+    for(string val:v)
+    {
+        cout<<val<<endl;
+    }
+    return 0;
+}
