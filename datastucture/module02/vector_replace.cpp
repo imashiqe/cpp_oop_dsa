4008f94ab7fe0772c54a3dcff27d0b8c65e87ad4
Ashiqur Rahman <imashiqe@gmail.com>
2024-06-13 14:50:48 +0600

    vector replace

---
+++ datastucture/module02/vector_replace.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+
+{
+    vector<int>v={1,2,3,4,5,1,2,4,5,3,2};
+     replace(v.begin(),v.end(),2,100);
+     for(int x:v)
+     {
+        cout<<x<<" ";
+     }
+
+    return 0;
+}
