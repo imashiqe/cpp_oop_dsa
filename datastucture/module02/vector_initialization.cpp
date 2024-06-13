b9d4161da6970aa5b0b6f0e1fe41858a0003d21f
Ashiqur Rahman <imashiqe@gmail.com>
2024-06-13 13:04:44 +0600

    Vector Capacity Functions

--- datastucture/module02/vector_initialization.cpp
+++ datastucture/module02/vector_initialization.cpp
@@ -4,19 +4,23 @@ using namespace std;
 
 int main()
 {
-    // vector<int> v; type 1
-    // vector<int> v(5); type 2
-    // vector<int> v(5,10); type 3
-    // vector<int> v2(5,100); type 4
-    // vector<int> v(v2);
-//  int a[6]={1,2,3,4,5,6};
-// vector<int> v(a,a+6);
- vector(int) v={2,10,3};
-   for(int i=0; i<v.size(); i++)
-   {
-    cout<<v[i]<<" ";
-   }
+    // Example vector initialization
+    // vector<int> v; // type 1
+    // vector<int> v(5); // type 2
+    // vector<int> v(5,10); // type 3
+    // vector<int> v2(5,100); // type 4
+    // vector<int> v(v2); // type 5
+    // int a[6]={1,2,3,4,5,6}; // array initialization
+    // vector<int> v(a,a+6); // vector from array
 
-    cout<<endl;
+    vector<int> v={2,10,3}; // current working vector initialization
+
+    // Output vector elements
+    for(int i=0; i<v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+
+    cout << endl;
     return 0;
 }
