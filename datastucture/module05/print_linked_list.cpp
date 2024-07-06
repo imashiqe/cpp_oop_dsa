638740e521931364fbe1f48b9e29a95cb2792d8c
Ashiqur Rahman <imashiqe@gmail.com>
2024-07-06 16:16:01 +0600

    Printing Singly Linked List

---
+++ datastucture/module05/print_linked_list.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+   public:
+   int val;
+   Node* next;
+   Node(int val)
+   {
+      this->val=val;
+      this->next=NULL;
+   }
+};
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* d = new Node(50);
+   
+    head->next = a;
+    a->next = b;
+    b->next = c;
+    c->next = d;
+
+    // cout<<head->val<<endl;
+    // cout<<head->next->val<<endl;
+    // cout<<head->next->next->val<<endl;
+    // cout<<head->next->next->next->val<<endl;
+
+    // while(head!=NULL)
+    // {
+    //     cout<<head->val<<endl;
+    //     head=head->next;
+    // }
+
+    Node* temp = head;
+    while(temp!=NULL)
+    {
+        cout<<temp->val<<endl;
+        temp=temp->next;
+    }
+
+    return 0;
+      
+}
